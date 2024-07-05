fe3e6295f277b9a3930f60e825b5c339d1e213c6
vitor277 <vitor-gasques@hotmail.com>
2024-07-05 13:50:16 -0300

    Iniciando projeto

---
+++ src/main/java/me/dio/SantanderDevApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevApplication.class, args);
+	}
+
+}
